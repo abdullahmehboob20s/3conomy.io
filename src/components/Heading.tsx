f0f5cee47e608aa6eefa42e09f2bc87a8379f885
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2024-06-10 14:10:30 +0500

    fix Heading lint issue

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,6 +1,10 @@
 import { cn } from "@/lib/utils";
 
-interface HeadingProps extends HTMLHeadElement {}
+interface HeadingProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLHeadingElement>,
+    HTMLHeadingElement
+  > {}
 
 function Heading({ className, ...props }: HeadingProps) {
   return <h2 className={cn("text-2xl 2xl:text-3xl", className)} {...props} />;
