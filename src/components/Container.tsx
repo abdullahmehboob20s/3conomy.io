f3dc5acf6c56f1db652bd42b7e550f7d91e7d241
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2024-06-10 14:08:48 +0500

    fix container lint issue

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,10 @@
 import { cn } from "@/lib/utils";
 
-interface ContainerProps extends HTMLDivElement {}
+interface ContainerProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {}
 
 function Container({ className, ...props }: ContainerProps) {
   return <div className={cn("p-6 lg:p-8 2xl:p-10", className)} {...props} />;
